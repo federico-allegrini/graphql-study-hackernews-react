9ce769fd2ab486e17664cfbb7faf82a406514548
Federico Allegrini <federico.allegrini.dev@gmail.com>
2021-01-11 16:51:52 +0100

    Setup apollo client

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/index.css";
+
+const httpLink = createHttpLink({ uri: "http://localhost:4000" });
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
